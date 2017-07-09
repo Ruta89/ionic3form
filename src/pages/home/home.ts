7e89f18e137f119a462e05dcd9eb976bf130bbe7
Arek Ruta <arek.ruta89@gmail.com>
2017-07-09 20:40:14 +0200

    used FormControl and FormGroup
     ReactiveFormsModule
    FormControl and FormGroup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
   selector: 'page-home',
@@ -7,13 +8,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   userAcc = { preferences: {} };
+  user: FormGroup;
 
   constructor(public navCtrl: NavController) {
-
+    this.user = new FormGroup({
+      username: new FormControl('Arek'),
+      email: new FormControl('are@arek.pl'),
+      password: new FormControl('123456'),
+      preferences: new FormGroup({
+        date: new FormControl(),
+        notes: new FormControl('Moje notatki!')
+      })
+    });
   }
 
   submitForm() {
     console.log(this.userAcc);
   }
 
+  formSubmit({ value, valid }: { value: User, valid: boolean }) {
+    console.log('user:  ', value);
+    console.log('valid:  ', valid);
+  }
+
+}
+
+export interface User {
+  username: string;
+  email: string;
+  password: string;
+  preferences: {
+    date: Date;
+    notes: string;
+  }
 }
